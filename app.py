ac4650cdea97bf38e532e0e75aa96b81b4edae1d
jiujiuxr <xrzhou026@gmail.com>
2024-11-18 07:23:34 +0000

    1

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
 from flask import Flask
+from flask import render_template,request
+
+app=Flask(_name_)
+
+app.route("/",methods=["GET","POST"])
+def index():
+    return(render_template("index.html"))
+
+if _name_=="_main_":
+    app.run()
